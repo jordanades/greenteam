ece2cba2c882f3197ab02bfb7a1c93b150e61a51
Jordan Ades <jrades44@gmail.com>
2024-04-12 10:35:17 -0400

    Basic structure for April 12

---
+++ final_project.py
@@ -0,0 +1,46 @@
+"""Write your function where I put your name. Commit to github when you
+finish your part.
+"""
+
+class RecipeManager:
+    def __init__(self):
+        """Jordan
+        """
+        self.recipes = []
+        
+    def search_for_ingredient(self, ingredient):
+        """Jamie
+        This method will search through the recipes and return a list of
+        recipes that contain the given incredient
+        """
+    def categorize_recipes(self):
+        """Ikenna
+        This method will search through the recipes and return a list of
+        lists that contain the recipes for the different categories
+        (list for desserts, for appetizers, etc.)"""
+
+class Recipe:
+    def __init__(self):
+        """Jordan
+        """
+        self.ingredients = []
+        self.directions = ""
+        self.category = "Recipe"
+    
+class ShoppingList:
+    def __init__(self):
+        """Jordan
+        """
+        self.shopping_list = []
+    def add_to_list(self, Recipe):
+        """Jesse
+        This method will take a Recipe object as imput and will search through
+        it's list of ingredients, and add the ingredients not already on the 
+        shopping list to the shopping list.
+        """
+        
+def view_recipe(Recipe):
+    """Anthony
+    Takes a recipe object as an imput and displays all of its properties in a
+    good looking format.
+    """
